fae3b662ecc14c61c4e8849a154b24469cede6a1
Juho <jeuslee99@naver.com>
2023-11-30 05:28:58 +0900

    AVL Tree

--- AVL.h
+++ AVL.h
@@ -22,12 +22,24 @@ public:
           cout << x->key << " " << x->depth << "\n";
      }
 
-     void minimum(AVLTreeNode* x) {
+     AVLTreeNode* minimum(AVLTreeNode* x) {
           //x의 subtree 중 가장 작은 node의 key
+          AVLTreeNode* tempNode = x;
+          while (tempNode->left != nullptr) {
+               tempNode = tempNode->left;
+          }
+
+          return tempNode;
      }
 
-     void maximum(AVLTreeNode* x) {
+     AVLTreeNode* maximum(AVLTreeNode* x) {
           //x의 subtree 중 가장 큰 node의 key
+          AVLTreeNode* tempNode = x;
+          while (tempNode->right != nullptr) {
+               tempNode = tempNode->right;
+          }
+
+          return tempNode;
      }
 
      //set이 비어 있으면 1을 출력하고, 비어 있지 않으면 0을 출력
@@ -59,40 +71,120 @@ public:
      }*/
 
 
-     //key가 x인 node의 depth를 출력
-     AVLTreeNode* find(int x) {
-
+     //key가 k인 node
+     AVLTreeNode* find(AVLTreeNode*  x, int k) {
+          if (x == nullptr || x->key == k) {
+               return x;
+          }
+          if (x->key > k) {
+               return find(x->left, k);
+          }
+          else {
+               return find(x->right, k);
+          }
      }
 
-     void insert(int x) {
+     AVLTreeNode* insert(AVLTreeNode* N, int k) {
           //내부에서 LL, RR, LR, RL 회전 진행
           //node를 insert 할 때마다 tree_size를 증가시켜주어야 함
+          if (root == NULL) {
+               AVLTreeNode* newNode;
 
+               newNode->key = k;
+               newNode->depth = 1;
+               newNode->left = NULL;
+               newNode->right = NULL;
+
+               return newNode;
+          }
+
+          if (k < root->key) {
+               root->left = insert(root->left, k);
+          }
+          else if (k > root->key) {
+               root -> right = insert(root-> right, k)
+          }
+          else {
+               return root
+          }
+
+          root->depth = max(root->left->depth, root->right->depth);
+
+          int temp = (root == NULL) ? 0 : root->left->depth - root->right->depth;
+          
+          if (k < root->left->key && temp >1) {
+               return rotate_ll(root);
+          }
+          else if (k > root->right->key && temp < -1) {
+               return rotate_rr(root);
+          }
+          else if (k > root->left->key && temp > 1) {
+               return rotate_lr(root);
+          }
+          else {
+               return rotate_rl(root);
+          }
+
+          return root;
      }
 
      //balance가 망가진 기준 노드를 A, 그 자식 노드를 B라고 가정.
      //LL 회전
-     AVLTreeNode* rotate_ll(AVLTreeNode* node) {
+     AVLTreeNode* rotate_ll(AVLTreeNode* A) {
           //B의 오른쪽 자식 = A
           //A의 왼쪽 자식 = B의 원래 오른쪽 자식
+          AVLTreeNode *B = A->left;
+          AVLTreeNode* C = B->right;
+
+          B->right = A;
+          A->left = C;
+
+          //A,B의 높이 갱신
+          A->depth = max(A->left->depth, A->right->depth)+ 1;
+          B->depth = max(B->left->depth, B->right->depth) + 1;
+
+
+          return B;
      }
 
      //LR 회전법
-     AVLTreeNode* rotate_lr(AVLTreeNode* node) {
+     AVLTreeNode* rotate_lr(AVLTreeNode* A) {
           //A의 왼쪽 자식 = RR(B)
           //LL(A)
+          AVLTreeNode* B = A->left;
+          B = rotate_rr(B);
+          rotate_ll(A);
+
+          return B;
      }
 
      //RL 회전법
-     AVLTreeNode* rotate_rl(AVLTreeNode* node) {
+     AVLTreeNode* rotate_rl(AVLTreeNode* A) {
           //A의 오른쪽 자식 = LL(B)
           //RR(A)
+          AVLTreeNode* B = A->right;
+          A = rotate_ll(B);
+          rotate_rr(A);
+
+          return B;
      }
 
      //RR 회전법
-     AVLTreeNode* rotate_rr(AVLTreeNode* node) {
+     AVLTreeNode* rotate_rr(AVLTreeNode* A) {
           //B의 왼쪽 자식 = A
           //A의 오른쪽 자식 = B의 원래 왼쪽 자식
+          AVLTreeNode* B = A->right;
+          AVLTreeNode* C = B->left;
+
+          B->left = A;
+          A->right = C;
+
+          //A,B의 높이 갱신
+          A->depth = max(A->left->depth, A->right->depth) + 1;
+          B->depth = max(B->left->depth, B->right->depth) + 1;
+
+
+          return B;
      }
 
 private:
